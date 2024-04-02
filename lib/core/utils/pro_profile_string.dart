4ccc3b6ce70cfe779efa7a6503b9408b409126c9
Soumya <soumyamishra637@gmail.com>
2024-04-02 14:22:17 +0530

    changes in yaml

--- lib/core/utils/pro_profile_string.dart
+++ lib/core/utils/pro_profile_string.dart
@@ -1,1 +1,16 @@
+class ProProfileStrings {
+  static const String email = "jackson35@outlook.com";
+  static const String contactMe = "Contact Me";
+  static const String contactMeNow = "Contact Me Now";
+  static const String designBy = "Design by ";
+  static const String featuredWork = "Featured Work";
+  static const String whyMe = "Why Me?";
+  static const String yourLogo = "Your Logo";
 
+  // Messages
+  static const String copyright2022 = " — Copyright 2022";
+  static const String copyRightMsg = "Design by $email — Copyright 2022";
+  static const String header = "Hi, I am David\nA Product Designer \nbased in Los Angeles.";
+  static const String desc1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis a interdum interdum venenatis et metus ante.";
+  static const String desc2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh est eget non pharetra tincidunt.";
+}
--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:pro_profile/core/pro_profile_export.dart';
+import 'package:pro_profile/core/utils/pro_profile_string.dart';
+import 'package:pro_profile/widgets/pro_profile_elevated_button.dart';
 import 'models/home_model.dart';
 import 'package:flutter/material.dart';
 import 'bloc/home_bloc.dart';
@@ -20,14 +22,68 @@ class HomeScreen extends StatelessWidget {
       child: Scaffold(
         body: SizedBox(
           width: MediaQuery.of(context).size.width,
-          child: const SingleChildScrollView(
+          child: SingleChildScrollView(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: [],
+              children: [
+                _buildAlexaIntroduction(context)
+              ],
             ),
           ),
         ),
       ),
     );
   }
+
+  Widget _buildAlexaIntroduction(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Text(
+                ProProfileStrings.header,
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center,
+                style: ProProfileTextStyles.di.copyWith(
+                  height: 1,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.only(left: 12, right: 12),
+              child: Text(
+                ProProfileStrings.desc1,
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center,
+                style: theme.textTheme.titleMedium!.copyWith(
+                  height: 1.44,
+                ),
+              ),
+            ),
+            const SizedBox(height: 30.0),
+            ProProfileElevatedButton(
+              text: ProProfileStrings.contactMe,
+              leftIcon: Container(
+                margin: const EdgeInsets.only(right: 15.0),
+                child: ProProfileImageView(
+                  imagePath: ProProfileImageConstant.imgLock,
+                  height: 15.0,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
 }
